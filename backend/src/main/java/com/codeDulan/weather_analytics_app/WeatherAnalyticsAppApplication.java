c1bf695395cdc5e139fd3a0f8ac47258eeee7b67
codeDulan <dulansamarasingha138@gmail.com>
2026-09-07 08:43:18 +0530

    intial backend

---
+++ backend/src/main/java/com/codeDulan/weather_analytics_app/WeatherAnalyticsAppApplication.java
@@ -0,0 +1,13 @@
+package com.codeDulan.weather_analytics_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAnalyticsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAnalyticsAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/codeDulan/weather_analytics_app/WeatherAnalyticsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeDulan.weather_analytics_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAnalyticsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
